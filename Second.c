c138ddc834a6b77bbe1bc68093a433eeca52d87d
WhiteeShadow <etimakbor404@gmail.com>
2024-10-27 16:57:33 +0500

    Second commit

---
+++ Second.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define MAX_NODES 100
+
+typedef struct Node {
+    int vertex;
+    int weight;
+    struct Node* next;
+} Node;
+
+typedef struct Graph {
+    int numVertices;
+    Node** adjLists;
+} Graph;
+
+typedef struct MinHeapNode {
+    int vertex;
+    int distance;
+} MinHeapNode;
+
+typedef struct MinHeap {
+    int size;
+    int capacity;
+    int* pos;
+    MinHeapNode** array;
+} MinHeap;
+
+// Function to create a new adjacency list node
+Node* createNode(int vertex, int weight) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->vertex = vertex;
+    newNode->weight = weight;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create a graph with V vertices
+Graph* createGraph(int vertices) {
+    Graph* graph = (Graph*)malloc(sizeof(Graph));
+    graph->numVertices = vertices;
+    graph->adjLists = (Node**)malloc(vertices * sizeof(Node*));
+    
+    for (int i = 0; i < vertices; i++)
+        graph->adjLists[i] = NULL;
+
+    return graph;
+}
+
+// Function to add an edge to a directed graph
+void addEdge(Graph* graph, int src, int dest, int weight) {
+    Node* newNode = createNode(dest, weight);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+}
+
+// Function to create a Min Heap node
+MinHeapNode* createMinHeapNode(int v, int dist) {
+    MinHeapNode* minHeapNode = (MinHeapNode*)malloc(sizeof(MinHeapNode));
+    minHeapNode->vertex = v;
+    minHeapNode->distance = dist;
+    return minHeapNode;
+}
+
+// Function to create a Min Heap
+MinHeap* createMinHeap(int capacity) {
+    MinHeap* minHeap = (MinHeap*)malloc(sizeof(MinHeap));
+    minHeap->pos = (int*)malloc(capacity * sizeof(int));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array = (MinHeapNode**)malloc(capacity * sizeof(MinHeapNode*));
+    return minHeap;
+}
+
+// Swap two nodes in the Min Heap
+void swapMinHeapNode(MinHeapNode** a, MinHeapNode** b) {
+    MinHeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Standard heapify function
+void minHeapify(MinHeap* minHeap, int idx) {
+    int smallest, left, right;
+    smallest = idx;
+    left = 2 * idx + 1;
+    right = 2 * idx + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->distance < minHeap->array[smallest]->distance)
+        smallest = left;
+
+    if (right < minHeap->size && minHeap->array[right]->distance < minHeap->array[smallest]->distance)
+        smallest = right;
+
+    if (smallest != idx) {
+        MinHeapNode* smallestNode = minHeap->array[smallest];
+        MinHeapNode* idxNode = minHeap->array[idx];
+
+        minHeap->pos[smallestNode->vertex] = idx;
+        minHeap->pos[idxNode->vertex] = smallest;
+
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+
+        minHeapify(minHeap, smallest);
+    }
+}
+
+
+
+
+
+// Main function
+int main() {
+    int vertices = 9;
+    Graph* graph = createGraph(vertices);
+
+    addEdge(graph, 0, 1, 4);
+    addEdge(graph, 0, 7, 8);
+    addEdge(graph, 1, 2, 8);
+    addEdge(graph, 1, 7, 11);
+    addEdge(graph, 2, 3, 7);
+    addEdge(graph, 2, 8, 2);
+    addEdge(graph, 2, 5, 4);
+    addEdge(graph, 3, 4, 9);
+    addEdge(graph, 3, 5, 14);
+    addEdge(graph, 4, 5, 10);
+    addEdge(graph, 5, 6, 2);
+    addEdge(graph, 6, 7, 1);
+    addEdge(graph, 6, 8, 6);
+    addEdge(graph, 7, 8, 7);
+
+ 
+
+    return 0;
+}
